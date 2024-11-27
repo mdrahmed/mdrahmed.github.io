50bfdeb22f75d346c5d68f511c4e49a3cb32fc9c
Md Raihan Ahmed <raihan.ahmed05.96@gmail.com>
2024-11-27 14:54:56 -0700

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,18 +2,18 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+// import leaf from "../../Assets/Projects/leaf.png";
+// import emotion from "../../Assets/Projects/emotion.png";
+// import editor from "../../Assets/Projects/codeEditor.png";
+// import chatify from "../../Assets/Projects/chatify.png";
+// import bitsOfCode from "../../Assets/Projects/blog.png";
 
 
 import icstracker from "../../Assets/projects_site/icstracker.png";
 import games from "../../Assets/projects_site/games.png";
 import xpdf from "../../Assets/projects_site/xpdf.png";
 import RollBack from "../../Assets/projects_site/rollback.jpeg";
+import suicide from "../../Assets/Projects/suicide.png";
 
 function Projects() {
   return (
